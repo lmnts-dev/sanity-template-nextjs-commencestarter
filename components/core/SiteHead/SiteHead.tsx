f0289efb8606ef514f9f42204035cae2f6d8b409
Jose Rios <jose@commence.studio>
2022-05-17 10:30:05 -0700

    :rocket: PWA Update

--- components/core/SiteHead/SiteHead.tsx
+++ components/core/SiteHead/SiteHead.tsx
@@ -74,6 +74,13 @@ export const SiteHead = ({
       ></meta>
       {/* end opengraph */}
 
+
+      <link rel="manifest" href={`/manifest.json`} />
+      <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+      <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      <meta name="theme-color" content="#000000"/>
+
       {/* schema */}
       {/*
       <script
